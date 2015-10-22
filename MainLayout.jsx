a115b77803fa1de32b389e0f38c0514d0e7805dc
ephetic <bpcleary@gmail.com>
2015-10-22 13:10:34 -0700

    [wip] basic css

--- MainLayout.jsx
+++ MainLayout.jsx
@@ -4,12 +4,13 @@ MainLayout = React.createClass({
     return (
       <div>
         <header className="topbar_header">
-          <h1 className="topbar_header_h1"> tuti </h1>
-          <AccountsUIWrapper
-            className="topbar_header_login" />
-          <a href="/page/home">Home</a>
-          {privs.admin || privs.teacher ? 
-            <a href="/admin">Admin</a> : ''}
+          <h1 className="topbar_header_h1">tuti</h1>
+          <div className="topbar_header_navbar">
+            <AccountsUIWrapper />
+            <a href="/page/home">Home</a>
+            {privs.admin || privs.teacher ? 
+              <a href="/admin">Admin</a> : ''}
+          </div>
         </header>
         <main>{this.props.content}</main>
         <footer>Made with Meteor © 2015 Brian Cleary</footer>
